f1bf0a2068dfc6980d2cfe2535cdf02a6f4f5dd7
Omar Hernández <127421759+omar15hr@users.noreply.github.com>
2025-08-28 11:56:41 -0400

    creamos proyecto, navbar, icons, estructura de carpetas

---
+++ src/app/components/layout/Navbar.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { CloseIcon, RightChevron, ToggleIcon } from "../Icons";
+
+export default function Navbar() {
+  const [state, setState] = useState(false);
+
+  const navigation = [
+    { title: "Features", path: "javascript:void(0)" },
+    { title: "Integrations", path: "javascript:void(0)" },
+    { title: "Customers", path: "javascript:void(0)" },
+    { title: "Pricing", path: "javascript:void(0)" },
+  ];
+
+  useEffect(() => {
+    document.onclick = (e) => {
+      const target = e.target;
+      if (!target.closest(".menu-btn")) setState(false);
+    };
+  }, []);
+
+  return (
+    <nav
+      className={`bg-white pb-5 md:text-sm ${
+        state
+          ? "shadow-lg rounded-xl border mx-2 mt-2 md:shadow-none md:border-none md:mx-2 md:mt-0"
+          : ""
+      }`}
+    >
+      <div className="gap-x-14 items-center max-w-screen-xl mx-auto px-4 md:flex md:px-8">
+        <div className="flex items-center justify-between py-5 md:block">
+          <a href="javascript:void(0)">
+            <Image
+              src="https://www.floatui.com/logo.svg"
+              width={120}
+              height={50}
+              alt="Float UI logo"
+            />
+          </a>
+          <div className="md:hidden">
+            <button
+              className="menu-btn text-gray-500 hover:text-gray-800"
+              onClick={() => setState(!state)}
+            >
+              {state ? <CloseIcon /> : <ToggleIcon />}
+            </button>
+          </div>
+        </div>
+        <div
+          className={`flex-1 items-center mt-8 md:mt-0 md:flex ${
+            state ? "block" : "hidden"
+          } `}
+        >
+          <ul className="justify-center items-center space-y-6 md:flex md:space-x-6 md:space-y-0">
+            {navigation.map((item, idx) => {
+              return (
+                <li key={idx} className="text-gray-700 hover:text-gray-900">
+                  <a href={item.path} className="block">
+                    {item.title}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+          <div className="flex-1 gap-x-6 items-center justify-end mt-6 space-y-6 md:flex md:space-y-0 md:mt-0">
+            <a
+              href="javascript:void(0)"
+              className="block text-gray-700 hover:text-gray-900"
+            >
+              Log in
+            </a>
+            <a
+              href="javascript:void(0)"
+              className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex"
+            >
+              Sign in
+              <RightChevron />
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
